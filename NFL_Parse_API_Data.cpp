c5cf1cbe3dcd4c70fd56deb74081ce5ab222c30f
Adeniyi Emiabata <denem@MacBook-Pro.local>
2025-06-01 01:09:21 -0500

    SQLite Database created with basic data for each player. Code comments to be included

--- NFL_Parse_API_Data.cpp
+++ NFL_Parse_API_Data.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <filesystem>
 #include <map>
+#include <sqlite3.h>
+#include <cstring>
 
 using namespace std;
 using json = nlohmann::json;
@@ -14,24 +16,6 @@ httplib::Client client_1("http://sports.core.api.espn.com");
 // Access 32 TEAMS first then ROSTER then PLAYER DATA
 string NFL_Teams_Path = "/v2/sports/football/leagues/nfl/teams?limit=32";
 
-string api_data = "full_api_data.txt";
-
-// check if the API data file and parsed data output file already exist, to avoid reprocessing
-bool Check_DataFiles_Size (string& filename){
-
-    if (filesystem::exists(filename) ){
-
-        if(filesystem::file_size(filename) == 108883){
-            cout << "Great! Welcome back!" << endl;
-            return true;
-        }
-
-        return false;
-
-    }
-    
-    return false;
-}
 
 // Parse API Data till players and store in a text file
 map<string, string> Data_Extraction(){
@@ -39,7 +23,6 @@ map<string, string> Data_Extraction(){
     // Return map:
     map <string,string> Team_to_Player_Data_Lookup;
     
-    ofstream Store_Here(api_data);
 
     auto Result_Teams = client_1.Get(NFL_Teams_Path);
 
@@ -58,7 +41,7 @@ map<string, string> Data_Extraction(){
         // get team http link
         string Team_Path = (j)["$ref"];
         Team_Path.erase(0, 31);
-        //cout << Team_Path << endl;
+       
         auto Result_Teams_Paths = client_1.Get(Team_Path);
 
         All_Teams_Data_Parsed.append(Result_Teams_Paths->body);
@@ -68,26 +51,24 @@ map<string, string> Data_Extraction(){
     All_Teams_Data_Parsed.erase(All_Teams_Data_Parsed.end()-1);
     All_Teams_Data_Parsed.append("]}");
 
-    cout << All_Teams_Data_Parsed << endl; // NO ISSUES HERE
+    
     // Parse to get each team's roster
     string Team_Roster_Data_Parsed;
-    //string append_to_path = "&limit="
-    // cout <<"here" << endl;
+  
     json Team_Rosters = json::parse(All_Teams_Data_Parsed);
-    //json Team_Rosters_v2 = json::parse(Team_Rosters["items"]);
+  
     int count = 0;
     
     for(auto j: Team_Rosters["team_data"]){
 
         string Team_Roster_Path = (j["athletes"])["$ref"];
         Team_Roster_Path.erase(0,31);
-        cout << Team_Roster_Path << endl; // CHECK
+        
         auto Result_Team_Roster_Data = client_1.Get(Team_Roster_Path);
 
         // find number of players to ensure the http path can display all players. &limit = _____
         json temp_find_count = json::parse(Result_Team_Roster_Data->body);
         int num_players = *temp_find_count.begin();
-        // num_players.erase(std::remove(num_players.begin(), num_players.end(), '\"'), num_players.end());
 
         Team_Roster_Path.append("&limit=");
         Team_Roster_Path.append(to_string(num_players));
@@ -95,7 +76,7 @@ map<string, string> Data_Extraction(){
         Result_Team_Roster_Data = client_1.Get(Team_Roster_Path);
         json get_team_name = json::parse(Result_Team_Roster_Data -> body);
         string team_name = j["displayName"];
-        cout << team_name << endl;
+    
         team_name.erase(std::remove(team_name.begin(), team_name.end(), '\"'), team_name.end());
         Team_to_Player_Data_Lookup[team_name] = Result_Team_Roster_Data->body;       
 
@@ -107,22 +88,15 @@ map<string, string> Data_Extraction(){
 
     Team_Roster_Data_Parsed.erase(Team_Roster_Data_Parsed.end()-1);
 
-    Store_Here << Team_Roster_Data_Parsed << endl;
-    Store_Here.close();
-
     return Team_to_Player_Data_Lookup;
     
 }
 
-string Get_Team_Name(map<string,string> Team_to_Player_Data_Lookup){
+void Validate_Team_Name(map<string,string> Team_to_Player_Data_Lookup, string Team_Name){
 
-    string Team_Name;
-
-    cout << "Enter Team Name: " << endl;
-    getline(cin, Team_Name);
 
     if (Team_to_Player_Data_Lookup.find(Team_Name) != Team_to_Player_Data_Lookup.end()){
-        return Team_Name;
+        return;
     }
 
     else{
@@ -155,7 +129,7 @@ map <string, string> Retrieve_Player_Info_For_Team(string Team_Name, map<string,
 }
 
 void Display_Player_Info (string Team_Name, map <string, string> PlayerName_PlayerData){
-    ////// HERE 05.08.2025
+   
     string Player_Name;
 
     cout << "Enter Player Name: " << endl;
@@ -176,41 +150,143 @@ void Display_Player_Info (string Team_Name, map <string, string> PlayerName_Play
     cout << "Position: " << (Player_Info["position"])["displayName"] << endl;
 
 }
-        
 
+string Remove_Quotes_from_String(string Data){
+    Data.erase(std::remove(Data.begin(), Data.end(), '\"'), Data.end());
 
-// // Get player name from user input
-// string Get_Player_Name(){
+    return Data;
+}
 
-//     //Get player name from user
-//     string PlayerName;
-//     std::cout << "What is the player's name?" << endl;
-//     getline(cin, PlayerName);
-//     std::cout << endl;
-    
-//     return PlayerName;
+string Escape_Character(string Player_Info){
+    string Output_String;
+    for(auto i: Player_Info){
+        if(i == '\''){
+            Output_String += '\'';
+        }
+        Output_String += i;
+    }
 
-// }
+    return Output_String;
+}
 
+map<string, string> Run_All_Parsing_Functions(string Team_Name){
 
+    map<string, string> Team_to_Player_Data_Lookup = Data_Extraction();
+
+    Validate_Team_Name(Team_to_Player_Data_Lookup, Team_Name);
+
+    map<string, string> PlayerName_Player_Data = Retrieve_Player_Info_For_Team(Team_Name, Team_to_Player_Data_Lookup);
+
+    return PlayerName_Player_Data;
+
+}
 
 int main(){
 
-    // Determine whether or not to GET data again
-    // bool Data_Exists = Check_DataFiles_Size(api_data);
-    // if(!Data_Exists){
-    //     Data_Extraction();
-    //     cout << "1" << endl;
-    // }
+    sqlite3* DB;
+    char* errmsg = nullptr;
 
-    map<string, string> FullData = Data_Extraction();
-    cout << "2" << endl;
-    string Team_Name = Get_Team_Name(FullData);
-    cout << "3" << endl;
-    map<string, string> PlayerName_Player_Data = Retrieve_Player_Info_For_Team(Team_Name, FullData);
-    cout << "4" << endl;
-    Display_Player_Info(Team_Name, PlayerName_Player_Data);
-    cout << "5" << endl;
+    if(sqlite3_open("Player_Data.db", &DB)){
+        cout << "DB cannot be opened: " << sqlite3_errmsg(DB) << endl;
+        exit(0);
+    }
+
+    const char* CreateSQLTable = "CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Position TEXT, Team TEXT, Age INTEGER, Debut_Year INTEGER, Years_of_Experience INTEGER);";
+
+    if(sqlite3_exec(DB, CreateSQLTable, nullptr, nullptr, &errmsg) != SQLITE_OK){
+        cout << "SQL Open Error: " << errmsg << endl;
+        sqlite3_free(errmsg);
+        exit(0);
+    }
+
+    vector<string> List_of_NFL_Teams = {
+    "Arizona Cardinals",
+    "Atlanta Falcons",
+    "Baltimore Ravens",
+    "Buffalo Bills",
+    "Carolina Panthers",
+    "Chicago Bears",
+    "Cincinnati Bengals",
+    "Cleveland Browns",
+    "Dallas Cowboys",
+    "Denver Broncos",
+    "Detroit Lions",
+    "Green Bay Packers",
+    "Houston Texans",
+    "Indianapolis Colts",
+    "Jacksonville Jaguars",
+    "Kansas City Chiefs",
+    "Los Angeles Chargers",
+    "Los Angeles Rams",
+    "Las Vegas Raiders",
+    "Miami Dolphins",
+    "Minnesota Vikings",
+    "New England Patriots",
+    "New Orleans Saints",
+    "New York Giants",
+    "New York Jets",
+    "Philadelphia Eagles",
+    "Pittsburgh Steelers",
+    "Seattle Seahawks",
+    "San Francisco 49ers",
+    "Tampa Bay Buccaneers",
+    "Tennessee Titans",
+    "Washington Commanders"
+    };
+
+    for(auto i: List_of_NFL_Teams){
+        map<string, string> PlayerName_Player_Data = Run_All_Parsing_Functions(i);
+
+        for(auto j: PlayerName_Player_Data){
+
+            auto Player_Info = client_1.Get(j.second);
     
+            json Player_Info_Parsed = json::parse(Player_Info->body);
+    
+            char insertSQL [500];
+    
+            // Escape in the event of apostrophe/single quote in player name
+            cout << "LINK LINK: " << j.second << endl;
+            string Player_Name =  Escape_Character(Remove_Quotes_from_String(Player_Info_Parsed["fullName"]));
+            string Player_Position = Remove_Quotes_from_String((Player_Info_Parsed["position"])["displayName"]);
+    
+            int age;
+            if(Player_Info_Parsed.contains("age")){
+                age = Player_Info_Parsed["age"].get<int>();
+            }
+            else{
+                age = -1;
+            }
+    
+            int draft_year;
+            int years_of_experience;
+            (Player_Info_Parsed["experience"])["years"].get<int>() > 0 ? years_of_experience = (Player_Info_Parsed["experience"])["years"].get<int>() - 1 : years_of_experience = (Player_Info_Parsed["experience"])["years"].get<int>();
+    
+            if(Player_Info_Parsed.contains("draft")){
+                draft_year = (Player_Info_Parsed["draft"])["year"].get<int>();
+            }
+            else{
+                draft_year = -1;
+            }
+    
+            cout << "Name: " << Player_Name << " Age: " << age << " Draft: " << draft_year << endl;
+    
+            snprintf(insertSQL, 500, "INSERT INTO Players (Name, Position, Team, Age, Debut_Year, Years_of_Experience) VALUES ('%s', '%s', '%s', %i, %i, %i);", Player_Name.c_str(), Player_Position.c_str(), i.c_str(), age, draft_year, years_of_experience);
+    
+            const char* insertSQL_const = insertSQL;
+    
+            if (sqlite3_exec(DB, insertSQL_const, nullptr, nullptr, &errmsg) != SQLITE_OK){
+                cout << "SQL Execution Error: " << errmsg << endl;
+                sqlite3_free(errmsg);
+                exit(0);
+            }
+        
+        }
+        
+    }
+
+    sqlite3_close(DB);
+   
     return 0;
+
 }
--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-g++ -std=c++23 -I/Users/denem/Documents/NFL_Players_Project/json/single_include -I/Users/denem/Documents/NFL_Players_Project/cpp-httplib -g NFL_Parse_API_Data.cpp -o NFL_Parse_API_Data
+g++ -std=c++23 -lsqlite3 -I/Users/denem/Documents/NFL_Players_Project/json/single_include -I/Users/denem/Documents/NFL_Players_Project/cpp-httplib -g NFL_Parse_API_Data.cpp -o NFL_Parse_API_Data
+
+./NFL_Parse_API_Data
