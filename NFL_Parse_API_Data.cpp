09d7bd4db85b147e03682b2c4278b8677c0631a2
Adeniyi Emiabata <denem@MacBook-Pro.local>
2025-06-01 13:43:55 -0500

    removed Display_player_info function, removed years of experience due to inconsistency in API, Added GET error messages, Added College Information, Added date for db creation in filename

--- NFL_Parse_API_Data.cpp
+++ NFL_Parse_API_Data.cpp
@@ -13,11 +13,9 @@ using namespace std;
 using json = nlohmann::json;
 
 httplib::Client client_1("http://sports.core.api.espn.com");
-// Access 32 TEAMS first then ROSTER then PLAYER DATA
+// Access 32 TEAMS first then ROSTER DATA then PLAYER DATA
 string NFL_Teams_Path = "/v2/sports/football/leagues/nfl/teams?limit=32";
 
-
-// Parse API Data till players and store in a text file
 map<string, string> Data_Extraction(){
 
     // Return map:
@@ -44,6 +42,12 @@ map<string, string> Data_Extraction(){
        
         auto Result_Teams_Paths = client_1.Get(Team_Path);
 
+        // END PROGRAM IF DATA IS NOT CORRECTLY GOTTEN
+        if (Result_Teams_Paths->status != 200){
+            std::cout << "Error getting API data!" << endl;
+            exit(0);
+        }
+
         All_Teams_Data_Parsed.append(Result_Teams_Paths->body);
         All_Teams_Data_Parsed.append(",");
     }
@@ -66,6 +70,12 @@ map<string, string> Data_Extraction(){
         
         auto Result_Team_Roster_Data = client_1.Get(Team_Roster_Path);
 
+        // END PROGRAM IF DATA IS NOT CORRECTLY GOTTEN
+        if (Result_Team_Roster_Data->status != 200){
+            std::cout << "Error getting API data!" << endl;
+            exit(0);
+        }
+
         // find number of players to ensure the http path can display all players. &limit = _____
         json temp_find_count = json::parse(Result_Team_Roster_Data->body);
         int num_players = *temp_find_count.begin();
@@ -74,6 +84,13 @@ map<string, string> Data_Extraction(){
         Team_Roster_Path.append(to_string(num_players));
 
         Result_Team_Roster_Data = client_1.Get(Team_Roster_Path);
+
+        // END PROGRAM IF DATA IS NOT CORRECTLY GOTTEN
+        if (Result_Team_Roster_Data->status != 200){
+            std::cout << "Error getting API data!" << endl;
+            exit(0);
+        }
+
         json get_team_name = json::parse(Result_Team_Roster_Data -> body);
         string team_name = j["displayName"];
     
@@ -119,6 +136,12 @@ map <string, string> Retrieve_Player_Info_For_Team(string Team_Name, map<string,
         
         auto Result_Player_Info = client_1.Get(Player_Info_Path);
 
+        // END PROGRAM IF DATA IS NOT CORRECTLY GOTTEN
+        if (Result_Player_Info->status != 200){
+            std::cout << "Error getting API data!" << endl;
+            exit(0);
+        }
+
         json Player_Info = json::parse(Result_Player_Info->body);
 
         PlayerName_PlayerData[Player_Info["displayName"]] = Player_Info_Path;
@@ -126,29 +149,6 @@ map <string, string> Retrieve_Player_Info_For_Team(string Team_Name, map<string,
     }
 
     return PlayerName_PlayerData;
-}
-
-void Display_Player_Info (string Team_Name, map <string, string> PlayerName_PlayerData){
-   
-    string Player_Name;
-
-    cout << "Enter Player Name: " << endl;
-    getline(cin, Player_Name);
-
-    string Player_Info_Path = PlayerName_PlayerData[Player_Name];
-
-    auto Result_Player_Info = client_1.Get(Player_Info_Path);
-
-    json Player_Info = json::parse(Result_Player_Info->body);
-
-    cout << "**********Player Bio**************" << endl << endl;
-    cout << "Player Name: " << Player_Name << endl;
-    cout << "Age: " << Player_Info["age"] << endl;
-    cout << "Height: " << Player_Info["displayHeight"] << endl;
-    cout << "Weight: " << Player_Info["displayWeight"] << endl;
-    cout << "Draft Year: " << Player_Info["debutYear"] << endl;
-    cout << "Position: " << (Player_Info["position"])["displayName"] << endl;
-
 }
 
 string Remove_Quotes_from_String(string Data){
@@ -186,15 +186,15 @@ int main(){
     sqlite3* DB;
     char* errmsg = nullptr;
 
-    if(sqlite3_open("Player_Data.db", &DB)){
+    if(sqlite3_open("Player_Data_01062025.db", &DB)){
         cout << "DB cannot be opened: " << sqlite3_errmsg(DB) << endl;
         exit(0);
     }
 
-    const char* CreateSQLTable = "CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Position TEXT, Team TEXT, Age INTEGER, Debut_Year INTEGER, Years_of_Experience INTEGER);";
+    const char* CreateSQLTable = "CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Position TEXT, Team TEXT, Age INTEGER, Draft_Year INTEGER, College TEXT);";
 
     if(sqlite3_exec(DB, CreateSQLTable, nullptr, nullptr, &errmsg) != SQLITE_OK){
-        cout << "SQL Open Error: " << errmsg << endl;
+        cout << "SQL Create Error: " << errmsg << endl;
         sqlite3_free(errmsg);
         exit(0);
     }
@@ -240,13 +240,19 @@ int main(){
         for(auto j: PlayerName_Player_Data){
 
             auto Player_Info = client_1.Get(j.second);
+
+            // END PROGRAM IF DATA IS NOT CORRECTLY GOTTEN
+            if (Player_Info->status != 200){
+                std::cout << "Error getting API data!" << endl;
+                exit(0);
+            }
     
             json Player_Info_Parsed = json::parse(Player_Info->body);
     
             char insertSQL [500];
     
             // Escape in the event of apostrophe/single quote in player name
-            cout << "LINK LINK: " << j.second << endl;
+            
             string Player_Name =  Escape_Character(Remove_Quotes_from_String(Player_Info_Parsed["fullName"]));
             string Player_Position = Remove_Quotes_from_String((Player_Info_Parsed["position"])["displayName"]);
     
@@ -259,8 +265,7 @@ int main(){
             }
     
             int draft_year;
-            int years_of_experience;
-            (Player_Info_Parsed["experience"])["years"].get<int>() > 0 ? years_of_experience = (Player_Info_Parsed["experience"])["years"].get<int>() - 1 : years_of_experience = (Player_Info_Parsed["experience"])["years"].get<int>();
+            
     
             if(Player_Info_Parsed.contains("draft")){
                 draft_year = (Player_Info_Parsed["draft"])["year"].get<int>();
@@ -268,10 +273,27 @@ int main(){
             else{
                 draft_year = -1;
             }
-    
-            cout << "Name: " << Player_Name << " Age: " << age << " Draft: " << draft_year << endl;
-    
-            snprintf(insertSQL, 500, "INSERT INTO Players (Name, Position, Team, Age, Debut_Year, Years_of_Experience) VALUES ('%s', '%s', '%s', %i, %i, %i);", Player_Name.c_str(), Player_Position.c_str(), i.c_str(), age, draft_year, years_of_experience);
+            
+            string college;
+            if(Player_Info_Parsed.contains("college")){
+                string College_Path = (Player_Info_Parsed["college"])["$ref"];
+                auto College_Info = client_1.Get(College_Path.erase(0,31));
+
+                // END PROGRAM IF DATA IS NOT CORRECTLY GOTTEN
+                if (College_Info->status != 200){
+                    std::cout << "Error getting API data!" << endl;
+                    exit(0);
+                }
+
+                json College_Parsed = json::parse(College_Info->body);
+                college = Escape_Character(Remove_Quotes_from_String(College_Parsed["name"]));
+            }
+            else{
+                college = "Information not Available!";
+            }
+
+
+            snprintf(insertSQL, 500, "INSERT INTO Players (Name, Position, Team, Age, Draft_Year, College) VALUES ('%s', '%s', '%s', %i, %i, '%s');", Player_Name.c_str(), Player_Position.c_str(), i.c_str(), age, draft_year, college.c_str());
     
             const char* insertSQL_const = insertSQL;
     
